89b2d226a2857ae52c2582f41852476a15f84b7e
Kim Brugger <kbr@brugger.dk>
2016-07-11 12:47:17 +0100

    redone the bulk script to do some basic checking of input

--- bulk_design.py
+++ bulk_design.py
@@ -1,22 +1,64 @@
-import re
+#!/usr/bin/python
+# 
+# 
+# 
+# 
+# Kim Brugger (11 Jul 2016), contact: kim@brugger.dk
+
+import sys
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 import os
 import sys
-import pipeliners as pipe
+import re
+#import pipeliners as pipe
+
+import subprocess
+def system_call( cmd ):
+
+    try:
+        subprocess.check_call(cmd, shell=True)
+
+    except subprocess.CalledProcessError as scall:
+        print "System call '%s' failed. Exit code: %s Error output: %s" %( cmd,  scall.returncode, scall.output)
+        exit()
+
+
 
 # Define system call
 
+if ( len(sys.argv ) == 1 ):
+	print "USAGE: %s input-file"
+	print "input-file one tab seperated output-name and region of interest pr line"
+	exit()
+
 infile = sys.argv[1]
+if ( infile.find(".txt") == -1):
+	print "Infile '%s' does not look like a txt file" % infile
+	exit()
+
+outfile = re.sub(r'.txt', '.zip', infile)
 
 with open( infile , "rU" ) as plist:
 
 	for line in plist:
-		line = line.strip("\n")
 
-		testname = re.sub(r"(.*)\t.*", r"\1", line)
-		chro = re.sub(r".*\t(.*):.*",r"\1",line)
-		npos = re.sub(r".*\t.*:(.*)",r"\1",line)
-                (testname, chro_pos) = line.split('\t')
-                (chro, npos) = chro_pos.split(':')
-	        cmd = "primer_designer -c " + chro + " -p " + npos + " -o " + testname
-		print cmd
-#		print pipe.system_call("Running design_primers", cmd )
+		line = line.strip("\n")
+		if ( line.find("\t") == -1):
+			print "'%s' does not contain a 'tab'";
+			next
+
+		(testname, region) = line.split("\t")
+		if ( region.find(":") == -1 ):
+			print "'%s' does not contain a ':'";
+			next
+
+		(chrom, npos) = region.split(":")
+	        cmd = "/software/packages/primer_designer/primer_designer_region.py -c " + chrom + " -p " + npos + " -o " + testname
+#		print cmd
+		system_call(cmd )
+
+
+system_call( "zip -j %s *pdf" % outfile )
+
+print "Output file: %s" % outfile
